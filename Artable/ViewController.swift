26682c78b91eef2b90da8ddb62e5381c89994846
Yulia <Gnedykhju@mail.ru>
2019-08-31 11:29:51 +0300

    Initial Commit

---
+++ Artable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artable
+//
+//  Created by User on 31/08/2019.
+//  Copyright © 2019 Yulia Gnedykh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
